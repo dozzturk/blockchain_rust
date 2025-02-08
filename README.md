a8e960e41a9074a24f55aaca3a7a108ae538a6de
Deniz Ozturk <dozturk@ncsu.edu>
2025-02-07 19:46:14 -0500

    mining function implemented

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 hex crate: https://crates.io/crates/hex
 crypto-hash crate: https://crates.io/crates/crypto-hash
 
+#NOTES#
 
 Hashing: SHA-256 is used - 32-byte hash
 
-
-Difficulty: (in this case) specifies the unsigned 128-bit integer that the most significant 16 bytes of the hash of a block must be less than before it is considered "valid"
+#Difficulty: (in this case) specifies the unsigned 128-bit integer that the most significant 16 bytes of the hash of a block must be less than before it is considered "valid"
 
 - Difficulty could also be expressed as:
 a. the first n bytes of the hash that must be zero
@@ -13,3 +13,22 @@ b. the number of bits or bytes at the beginning of the hash that must be zero
     
 Check difficulty function implemented:
 - Converting hash 16 bytes to an unsigned 128 bit integer and then doing > comparison.
+
+#Nonce:
+- unique to the data
+- reproducible fingerprint for some data
+- Therefore, to make a "valid" hash (per difficulty), we must change the bytes we send to the function (pre-image).
+- Since we can't actually change the information stored in a block, we introduce an additional piece of data called a nonce: an arbitrary (but not necessarily random) value added as a field to each block, and hashed along with the data. 
+- Since it has been declared arbitrary, we can change it as we please.
+- Think of it like this: generating the correct hash for a block is like the puzzle, and the nonce is the key to that puzzle. The process of finding that key is called "mining".
+
+#Mining Strategy: (Algorithm)
+1. Generate new nonce
+2. Hash bytes (this is the computationally heavy step)
+3. Check hash against difficulty: 
+    - a. Insufficient? -> Go back to step 1
+    - b. Sufficient? -> Continue to step 4
+4. Add block to chain
+5. Submit to others (Networking is needed for this step but this is not implemented yet).
+
+
--- src/block.rs
+++ src/block.rs
@@ -9,6 +9,7 @@ pub struct Block {
     pub prev_block_hash: BlockHash,
     pub nonce: u64, // nonce is a random number that is used only once in a blockchain to create a block.
     pub payload: String, // Later this will be changed to transactions
+    pub difficulty: u128
 }
 
 impl Debug for Block {
@@ -23,7 +24,7 @@ impl Debug for Block {
 }
 
 impl Block {
-    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64,  payload: String) -> Self { // -> returns Block
+    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64,  payload: String, difficulty: u128) -> Self { // -> returns Block
         return Block { 
                 index,
                 timestamp,
@@ -31,9 +32,27 @@ impl Block {
                 prev_block_hash,
                 nonce,
                 payload,
+                difficulty,
+        }
+    }
+
+    // Mining function
+    pub fn mine (&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            // loop though until find one
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+
+            //check hash to see if it matches the difficulty
+            if check_difficulty(&hash, self.difficulty) {
+                // if this returns true, then hash matches the difficulty
+                self.hash = hash;
+                return;
+            }
         }
     }
 }
+
 
 // Hash function - takes &self as a parameter and returns vector of bytes (Vec<u8>)
 impl Hashable for Block {
@@ -44,6 +63,7 @@ impl Hashable for Block {
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce)); // uses pre-defined functions
         bytes.extend(self.payload.as_bytes()); //uses .as_bytes() because payload is String
+        bytes.extend(&u128_bytes(&self.difficulty)); // uses pre-defined functions
 
         return bytes
     }
